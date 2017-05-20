7f5a644bb56c841c7b50f7ae75a05d7916b4a0dd
Zhas1ke <zhassulan.shakenov@gmail.com>
2017-05-20 18:50:47 +0600

    some changes

--- task/w0.py
+++ task/w0.py
@@ -7,7 +7,17 @@ def generate_numbers():
 		lst.append(random())
 	return pd.DataFrame(lst)
 
-for i in range(10):
-	df = generate_numbers()
-	filename = 'files/file_' + str(i) + '.csv'
-	df.to_csv(filename, sep = ',', header = None, index = None)
+# for i in range(10):
+# 	df = generate_numbers()
+# 	filename = 'files/file_' + str(i) + '.csv'
+# 	df.to_csv(filename, sep = ',', header = None, index = None)
+
+from datetime import date
+import random
+
+start_date = date.today().replace(day=1, month=1).toordinal()
+end_date = date.today().toordinal()
+random_day = date.fromordinal(random.randint(start_date, end_date))
+
+print (random_day)
+print (type(random_day))
